65102f751568493a53d9d1c0e9a0a5f05b0a9580
Evodiatembi <evodytembi@gmail.com>
2024-08-02 18:53:40 +0000

    0-app.py, templates/0-index.html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-""" 0. Basic Flask app. """
-from flask import Flask, render_template
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def welcome():
-    """ / page """
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
